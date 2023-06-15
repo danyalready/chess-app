24d1d14787ad6580e6ebdcc71dd36bf6f1e15ef8
danyalready <doniyorgit@gmail.com>
2023-06-15 20:35:52 +0500

    fix: add rook offsets

--- src/constants/chess.ts
+++ src/constants/chess.ts
@@ -21,6 +21,13 @@ export const BISHOP_OFFSETS = [
     [1, 1],
 ];
 
+export const ROOK_OFFSETS = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+];
+
 export const KING_OFFSETS = [
     [-1, -1],
     [-1, 0],
