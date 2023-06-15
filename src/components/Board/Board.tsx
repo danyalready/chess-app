eada279359eb5234be0af753b972f84ab6c76554
danyalready <doniyorgit@gmail.com>
2023-06-16 01:23:10 +0500

    feat: unslect square on click outside

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { getPieceEmoji } from 'src/utils/chess';
 import * as css from 'src/utils/css';
@@ -8,6 +8,7 @@ import { selectSquare, unselectSquare } from 'src/actions/board';
 import './Board.css';
 
 const Board = () => {
+    const boardRef = useRef<HTMLDivElement>(null);
     const [{ board, selectedSquare }, dispatch] = useChess();
 
     useEffect(() => {
@@ -16,10 +17,16 @@ const Board = () => {
                 unselectSquare(dispatch);
             }
         });
+
+        window.addEventListener('click', (event) => {
+            if (!boardRef.current?.contains(event.target as Node)) {
+                unselectSquare(dispatch);
+            }
+        });
     }, [dispatch]);
 
     return (
-        <div className='board'>
+        <div className='board' ref={boardRef}>
             {board.map((row, rowIndex) => (
                 <div key={rowIndex} className='board-row'>
                     {row.map((square) => (
